576c7ecf8e2ba50a4fcda87d079338a507a0f24c
polly_stos <polly_stos@yahoo.com.br>
2023-12-06 12:31:26 -0300

    Projeto Piano Simulator DIO

---
+++ src/script/engine.js
@@ -0,0 +1,38 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
+
+let mapedkeys = [];
+let audio = new Audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`;
+    audio.play();
+
+    const clickKey = document.querySelector(`[data-key="${key}"]`);
+    clickKey.classList.add("active");
+    setTimeout(() => {
+        clickKey.classList.remove("active")
+    }, 150);
+};
+
+pianoKeys.forEach((key) => {
+    key.addEventListener("click", () => playTune(key.dataset.key));
+    mapedkeys.push(key.dataset.key)
+});
+
+document.addEventListener("keydown", (e) => {
+    if (mapedkeys.includes(e.key)) {
+        playTune(e.key);
+    }
+});
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+};
+showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+keysCheck.addEventListener("click", showHideKeys)
